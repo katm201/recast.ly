3136956acc16cf3f71687cc17f65e09140fbdb90
Hack Reactor Student <student@hackreactor.com>
2017-10-25 15:42:12 -0700

    Rendered App, Search, Player, and List. Working on getting individual list entries to render based on passed-in data.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // TODO: Render the `App` component to the DOM
 
+ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Search />, document.getElementById('search'));
+ReactDOM.render(<VideoPlayer />, document.getElementById('player'));
+ReactDOM.render(<VideoList videos={window.fakeVideoData}/>, document.getElementById('videoList'));
